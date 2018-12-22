45131bb16cd76724509d8234874a3d7798d2e32b
Benjamin Jones <bljones36@gmail.com>
2018-12-22 14:56:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,16 +6,16 @@ class App < Sinatra::Base
   params[:name].reverse
   end
 
-  get "/square/:number" do
-  @number = params[:number].to_i
-  @answer = number**2
-  @answer.to_s
-  end
-
   get "/say/:number/:phrase" do
   @number = params[:number].to_i
   @phrase = params[:phrase]
   @number.times {@phrase}
+  end
+  
+  get "/square/:number" do
+  @number = params[:number].to_i
+  @answer = number**2
+  @answer.to_s
   end
 
 end
