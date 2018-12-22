d1081ea70c882c2f8f1d9fc800c866d94ea3e50f
Benjamin Jones <bljones36@gmail.com>
2018-12-22 15:45:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,9 +25,10 @@ class App < Sinatra::Base
   end
 
   get "/:operation/:number1/:number2" do
+    operation = params[:operation]
     number1 = params[:number1].to_i
     number2 = params[:number2].to_i
-    answer = number1+number1
+    answer = "#{number1}#{operation}#{number2}"
   end
   
 end
